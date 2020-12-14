63441b5fda619de2277e9b9ecda47a7505c12893
LoicBruchez <32639117+LoicBruchez@users.noreply.github.com>
2020-12-14 16:15:09 +0100

    last version

--- create_npz.py
+++ create_npz.py
@@ -1,26 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# voxel morph imports
-import tensorflow as tf
-import voxelmorph as vxm
-import neurite as ne
-assert tf.__version__.startswith('2.'), 'This tutorial assumes Tensorflow 2.0+'
-
-import numpy as np
-import scipy.ndimage as ndi
 import h5py
-import cv2
 
-import utilitary as util
 from numpy import savez_compressed
-
-#plotting
-import matplotlib.pyplot as plt
+import numpy as np
 
 np.random.seed(336699)
 
-hf = h5py.File("../epfl3.h5", "r")
+hf = h5py.File("epfl3.h5", "r")
 
 nb_entries = len(hf.keys())
 list_keys = list(hf.keys())
@@ -29,46 +17,18 @@ keys_random = np.random.permutation(list_keys)
 keys_train = keys_random[:int(nb_entries*0.8)]
 keys_test  = keys_random[int(nb_entries*0.8):]
 
-#get the index of keys with frame + labels
-masked_train = [i for i in keys_train if (len(hf.get(i)) > 1)]
-
-#preallocation
-labels_train_3d_semisup = np.zeros((len(keys_train),112,112,32))
-slices_train_3d_semisup = np.zeros((len(keys_train),112,112,32))
-
-#load the training set + normalization
-for i, key in enumerate(keys_train):
-    if i in masked_train:
-        labels_train_3d_semisup[i] = np.array(hf.get(key)["mask"])
-        slices_train_3d_semisup[i] = np.array(hf.get(key)["frame"][0][:,:,:])/255
+#export the training set + normalization
+for key in list_keys:
+    #clamping
+    tmp=np.array(hf.get(key)["frame"])
+    tmp[np.where(tmp > 255)] = 255
+    
+    #save normalize frame and its mask
+    if len(hf.get(key)) > 1:
+    	savez_compressed('vol'+str(key)+'.npz',
+                     vol=tmp/255,
+                     seg=np.array(hf.get(key)["mask"]))
     else:
-        labels_train_3d_semisup[i] = np.zeros((112, 112,  32))
-        slices_train_3d_semisup[i] = np.array(hf.get(key)["frame"][0][:,:,:])/255
-        
-# generate a file for each semi-supervised training entry
-for i in range(len(slices_train_3d_semisup)):
-    savez_compressed('vol'+str(i)+'.npz',
-                     vol=slices_train_3d_semisup[i],
-                     seg=labels_train_3d_semisup[i])
-
-#get the index of keys with frame + labels
-masked_test = [i for i in keys_test if (len(hf.get(i)) > 1)]
-
-#preallocation
-labels_test_3d_semisup = np.zeros((len(keys_test),112,112,32))
-slices_test_3d_semisup = np.zeros((len(keys_test),112,112,32))
-
-#load the test set + normalization
-for i, key in enumerate(keys_test[:51]):
-    if i in masked_test:
-        labels_test_3d_semisup[i] = np.array(hf.get(key)["mask"])
-        slices_test_3d_semisup[i] = np.array(hf.get(key)["frame"][0][:,:,:])/255
-    else:
-        labels_test_3d_semisup[i] = np.zeros((112, 112,  32))
-        slices_test_3d_semisup[i] = np.array(hf.get(key)["frame"][0][:,:,:])/255
-        
-# generate a file for each semi-supervised training entry
-for i in range(len(slices_test_3d_semisup)):
-    savez_compressed('val'+str(i)+'.npz',
-                     vol=slices_test_3d_semisup[i],
-                     seg=labels_test_3d_semisup[i])
+    	savez_compressed('vol'+str(key)+'.npz',
+                     vol=tmp/255,
+                     seg=np.zeros((112, 112,  32)))
