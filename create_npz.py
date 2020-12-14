d3e81af271eae9f1a9c7d9e86fbb4b87a305e1dd
LoicBruchez <32639117+LoicBruchez@users.noreply.github.com>
2020-12-14 20:41:10 +0100

    last commit

--- create_npz.py
+++ create_npz.py
@@ -6,29 +6,22 @@ import h5py
 from numpy import savez_compressed
 import numpy as np
 
-np.random.seed(336699)
-
 hf = h5py.File("../epfl3.h5", "r")
 
-nb_entries = len(hf.keys())
 list_keys = list(hf.keys())
-keys_random = np.random.permutation(list_keys)
-
-keys_train = keys_random[:int(nb_entries*0.8)]
-keys_test  = keys_random[int(nb_entries*0.8):]
 
 #export the training set + normalization
 for key in list_keys:
     #clamping
-    tmp=np.array(hf.get(key)["frame"])
+    tmp=np.array(hf.get(key)["frame"][0])
     tmp[np.where(tmp > 255)] = 255
     
     #save normalize frame and its mask
     if len(hf.get(key)) > 1:
     	savez_compressed('vol'+str(key)+'.npz',
-                     vol=tmp/255,
+                     vol=(tmp-93)/(255-93),
                      seg=np.array(hf.get(key)["mask"]))
     else:
     	savez_compressed('vol'+str(key)+'.npz',
-                     vol=tmp/255,
+                     vol=(tmp-93)/(255-93),
                      seg=np.zeros((112, 112,  32)))
